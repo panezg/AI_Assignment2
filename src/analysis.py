b1fa223fbee957ffc663bf65792a2dd3f0839f2f
gpanez <gustavo.panez.velazco@gmail.com>
2019-04-16 17:38:13 +0100

    Increasing number of functions and number of runs

--- src/analysis.py
+++ src/analysis.py
@@ -13,19 +13,24 @@ from SwarmPackagePy.animation import animation
 
 num_iterations = 100
 num_agents = 25
-num_runs = 10
+num_runs = 25
 list_error = []
 list_exec_time = []
 list_functions = [(testFunctions.ackley_function, (0.0, 0.0)),
-                  (testFunctions.easom_function, (math.pi, math.pi))]
+                  (testFunctions.easom_function, (math.pi, math.pi)),
+                  (testFunctions.sphere_function, (0.0 , 0.0)),
+                  (testFunctions.bohachevsky_function, (0.0 , 0.0)),
+                  (testFunctions.sum_squares_function, (0.0 , 0.0))]
 for l in range(len(list_functions)):
     # gets the function
     function = list_functions[l][0]
-    print("Function: " + ", " + str(function))
-    print("Run, Mean Error, Execution Time")
+    #print("Function: " + ", " + str(function))
+    #print("Run, Mean Error, Execution Time")
+    avg_error = 0
+    avg_delta = 0
     for i in range(num_runs):
         start = datetime.datetime.now()
-        alh = SwarmPackagePy.gsa(num_agents, function, -10, 10, 2, num_iterations)
+        alh = SwarmPackagePy.fa(num_agents, function, -10, 10, 2, num_iterations)
         end = datetime.datetime.now()
         delta = end - start
         last_list_pos = alh.get_agents()[-1]
@@ -41,6 +46,12 @@ for l in range(len(list_functions)):
         list_exec_time.append(delta.microseconds)
         #print("average error for run " + str(i + 1) + ": " + str(error))
         #print("exec time for run " + str(i + 1) + ": " + str(delta,microseconds))
-        print(str(i + 1) + ", " + str(error) + ", " + str(delta.microseconds))
+        #print(str(i + 1) + ", " + str(error) + ", " + str(delta.microseconds))
+        avg_error += error
+        avg_delta += delta.microseconds
         # Show animation
         # animation(alh.get_agents(), function, -10, 10, sr=True)
+    avg_error /= num_runs
+    avg_delta /= num_runs
+    #print("Averages of runs")
+    print(str(function) + ", " + str(avg_error) + ", " + str(avg_delta))
